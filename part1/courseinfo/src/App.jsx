ccba425014449144ecd12cf48050c9c3bb1477ed
iluvshiwoon <sntcillian@gmail.com>
2025-03-25 15:51:21 +0000

    1.2: Course Information, step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,18 +6,26 @@ const Header = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>
+            {props.part_name} {props.exercises_number}
+        </p>
+    )
+}
+
 const Content = (props) => {
-    return(
+    return (
         <>
-            <p>
-                {props.part} {props.exercises}
-            </p>
+            <Part part_name={props.part1} exercises_number={props.exercises1}/>
+            <Part part_name={props.part2} exercises_number={props.exercises2}/>
+            <Part part_name={props.part3} exercises_number={props.exercises3}/>
         </>
     )
 }
 
 const Total = (props) => {
-    return(
+    return (
         <p>
             Number of exercises {props.total}
         </p>
@@ -36,9 +44,8 @@ const App = () => {
   return (
     <div>
         <Header name={course} />
-        <Content part={part1} exercises={exercises1} />
-        <Content part={part2} exercises={exercises2} />
-        <Content part={part3} exercises={exercises3} />
+        <Content part1={part1} part2={part2} part3={part3} 
+                exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
         <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
